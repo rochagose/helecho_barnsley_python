e46f40ed3abade1c10f154bdcef7cc0c8ee6d1e9
rochagose <rochacruzjosedejesus100@gmail.com>
2025-01-21 10:14:59 -0600

    Constantes y variables

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 import matplotlib
 import random
 
+#CONSTANTES DE TRANSFORMACION
+TRANSFORMACIONES = [
+    (0.00, 0.00, 0.00, 0.16, 0.00, 0.00, 0.01),  
+    (0.85, 0.04, -0.04, 0.85, 0.00, 1.60, 0.85), 
+    (0.20, -0.26, 0.23, 0.22, 0.00, 1.60, 0.07), 
+    (-0.15, 0.28, 0.26, 0.24, 0.00, 0.44, 0.07)  
+]
+
+#Variables
+x = 0
+y = 0
+pts = []
+
+
